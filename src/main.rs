5a83ee35e39ca6faefdf4418e0b668dd5f16f343
fj0lverkr <nils.nahooy@live.com>
2022-02-27 22:40:19 +0100

    started working with sway tree after a good refresher on Rust

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,42 @@
-fn next_workspace_number(conn: &mut swayipc::Connection) -> Result<i32, swayipc::Error> {
-    let workspaces = conn.get_workspaces()?;
-    let mut ids: Vec<i32> = workspaces.iter().map(|w| w.num).collect();
-    ids.sort_unstable();
-    let len = ids.len() as i32;
-    Ok(ids
-        .into_iter()
-        .enumerate()
-        .find(|&(idx, workspace_num)| idx as i32 + 1 != workspace_num)
-        .map_or(len + 1, |(idx, _)| idx as i32 + 1))
-}
-
 fn active_workspace_numbers(conn: &mut swayipc::Connection) -> Result<Vec<i32>, swayipc::Error> {
     let workspaces = conn.get_workspaces()?;
     Ok(workspaces.iter().map(|w| w.num).collect())
 }
 
+fn get_tree(conn: &mut swayipc::Connection) -> Result<swayipc::Node, swayipc::Error> {
+    let tree = conn.get_tree()?;
+    Ok(tree)
+}
+
 fn main() -> Result<(), swayipc::Error> {
+    use swayipc::NodeType::*;
+
     let mut conn = swayipc::Connection::new()?;
-    let next_ws = next_workspace_number(&mut conn)?;
     let active_ws = active_workspace_numbers(&mut conn)?;
+
     for ws in active_ws {
-        println!("WS in use: {}",ws);
+        println!("WS in use: {}", ws);
     }
-    println!("Next free WS: {}", next_ws);
+
+    let tree = get_tree(&mut conn)?;
+    // println!("{:?}", tree); //print out the tree to get a feel for the structure
+
+    for node in tree.nodes {
+        match node.node_type {
+            Root => println!("Root node."),
+            Workspace => match node.name {
+                Some(n) => println!("Workspace node {}.", n),
+                None => println!("Unnamed workspace node."),
+            },
+            Con => println!("Container node."),
+            Output => match node.name {
+                Some(n) => println!("Output node {}.", n),
+                None => println!("Output node."),
+            },
+            Dockarea => println!("Dock area node."),
+            FloatingCon => println!("Floating container node."),
+            _ => println!("Unknows node type."),
+        }
+    }
     Ok(())
 }
