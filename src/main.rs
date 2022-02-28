5a06d0399082616a09734ea1093ccf6a85a50f13
fj0lverkr <nils.nahooy@live.com>
2022-02-28 19:58:06 +0100

    played around with workspaces

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-fn active_workspace_numbers(conn: &mut swayipc::Connection) -> Result<Vec<i32>, swayipc::Error> {
+fn get_active_workspace_numbers(conn: &mut swayipc::Connection) -> Result<Vec<i32>, swayipc::Error> {
     let workspaces = conn.get_workspaces()?;
     Ok(workspaces.iter().map(|w| w.num).collect())
+}
+
+fn get_workspace_name(conn: &mut swayipc::Connection, id: i32) -> Result<String, swayipc::Error> {
+    let workspaces = conn.get_workspaces()?;
+    let mut name = String::new();
+    for w in workspaces {
+        if w.num == id {
+            name = w.name;
+        }
+    }
+    Ok(name)
 }
 
 fn get_tree(conn: &mut swayipc::Connection) -> Result<swayipc::Node, swayipc::Error> {
@@ -40,10 +51,13 @@ fn analyze_node(tree: &swayipc::Node) {
 
 fn main() -> Result<(), swayipc::Error> {
     let mut conn = swayipc::Connection::new()?;
-    let active_ws = active_workspace_numbers(&mut conn)?;
+    let active_ws_nums = get_active_workspace_numbers(&mut conn)?;
 
-    for ws in active_ws {
-        println!("WS in use: {}", ws);
+
+    for ws in active_ws_nums {
+        let ws_name = get_workspace_name(&mut conn, ws)?;
+        let command = format!("workspace --no-auto-back-and-forth {}", &ws_name);
+        conn.run_command(command)?;
     }
 
     let tree = get_tree(&mut conn)?;
