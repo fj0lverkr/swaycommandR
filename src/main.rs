99a22e3289b73be5be02c3a0d5d783e3a616f806
fj0lverkr <nils.nahooy@live.com>
2022-02-28 17:14:00 +0100

    moved tree analyzing to seperate function

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,28 @@ fn get_tree(conn: &mut swayipc::Connection) -> Result<swayipc::Node, swayipc::Er
     Ok(tree)
 }
 
-fn main() -> Result<(), swayipc::Error> {
+fn analyze_node (tree: &swayipc::Node) {
     use swayipc::NodeType::*;
+    for node in &tree.nodes{
+        match node.node_type {
+            Root => println!("Root node."),
+            Workspace => match &node.name {
+                Some(n) => println!("Workspace node {}.", n),
+                None => println!("Unnamed workspace node."),
+            },
+            Con => println!("Container node."),
+            Output => match &node.name {
+                Some(n) => println!("Output node {}.", n),
+                None => println!("Output node."),
+            },
+            Dockarea => println!("Dock area node."),
+            FloatingCon => println!("Floating container node."),
+            _ => println!("Unknows node type."),
+        }
+    }
+}
+
+fn main() -> Result<(), swayipc::Error> {
 
     let mut conn = swayipc::Connection::new()?;
     let active_ws = active_workspace_numbers(&mut conn)?;
@@ -20,23 +40,6 @@ fn main() -> Result<(), swayipc::Error> {
 
     let tree = get_tree(&mut conn)?;
     // println!("{:?}", tree); //print out the tree to get a feel for the structure
-
-    for node in tree.nodes {
-        match node.node_type {
-            Root => println!("Root node."),
-            Workspace => match node.name {
-                Some(n) => println!("Workspace node {}.", n),
-                None => println!("Unnamed workspace node."),
-            },
-            Con => println!("Container node."),
-            Output => match node.name {
-                Some(n) => println!("Output node {}.", n),
-                None => println!("Output node."),
-            },
-            Dockarea => println!("Dock area node."),
-            FloatingCon => println!("Floating container node."),
-            _ => println!("Unknows node type."),
-        }
-    }
+    analyze_node(&tree);
     Ok(())
 }
